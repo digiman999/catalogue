b396228a9a2a93b1918cdbc716a7a9148fcd6ec2
Eddie Howell <eddiejhowell@gmail.com>
2017-07-19 10:16:31 +0100

    First

---
+++ services/scm/catalogue/catalogue.py
@@ -0,0 +1,28 @@
+'''
+Try & be generic for all asset types: audio, video, image, product?
+    - cons:
+        - give redundant data/schemas
+Parameters
+    - type: audio, video, image, podcast etc
+    - parent: make them composable
+
+
+Queues
+    - <assetType>.created
+
+
+'''
+
+
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return 'catalogue home'
+
+
+app.run(debug=True)
